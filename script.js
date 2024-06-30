c62c556da6ec7de79a667a411c6403fb4302667c
Victor Santin <157748636+vsanlic@users.noreply.github.com>
2024-06-30 18:26:18 +0200

    Modify script.js to create the 1st functional version

--- script.js
+++ script.js
@@ -1,31 +1,98 @@
-let firstNum, operator, secondNum;
+let previousNum = "";
+let currentNum = "";
+let operator = "";
 
-function add(a, b) {
-    return a + b;
+document.addEventListener("DOMContentLoaded", () => {
+    let screen = document.querySelector(".screen");
+    let numbers = document.querySelectorAll(".number-button");
+    let operators = document.querySelectorAll(".operator-button");
+    let clear = document.querySelector("#clear-button");
+    let decimal = document.querySelector("#decimal-button");
+    let equalTo = document.querySelector("#equal-to-button");
+    
+    screen.textContent = "0";
+
+    numbers.forEach((number) => number.addEventListener("click", (e) => {
+        populateNumbers(e.target.textContent);
+        screen.textContent = currentNum;
+    }))
+
+    operators.forEach((op) => op.addEventListener("click", (e) => {
+        populateOperators(e.target.textContent);
+        screen.textContent = operator;
+    }))
+
+    equalTo.addEventListener("click", () => {
+        if (currentNum != "" && previousNum != "") {
+            operate();
+            if (previousNum <= 13) {
+                screen.textContent = previousNum;
+            } else if (previousNum > 13) {
+                screen.textContent = previousNum.slice(0, 13);
+            }
+            
+        }
+    })
+
+    clear.addEventListener("click", () => {
+        previousNum = "";
+        currentNum = "";
+        operator = "";
+        screen.textContent = "0";
+    })
+
+    decimal.addEventListener("click", () => {
+        addDecimal();
+    })
+})
+
+function populateNumbers(num) {
+    if (currentNum.length <= 13) {
+        currentNum += num;
+    }
 }
 
-function subtract(a, b) {
-    return a - b;
+function populateOperators(op) {
+    operator = op;
+    previousNum = currentNum;
+    currentNum = "";
 }
 
-function multiply(a, b) {
-    return a * b;
-}
+function operate() {
+    previousNum = Number(previousNum);
+    currentNum = Number(currentNum);
 
-function divide(a, b) {
-    return a / b;
-}
-
-function operate(a, b, operation) {
-    if (operation === 1) {
-        return add(a, b);
-    } else if (operation === 2) {
-        return subtract(a, b);
-    } else if (operation === 3) {
-        return multiply(a, b);
-    } else if (operation === 4) {
-        return divide(a, b);
+    if (operator === "+") {
+        previousNum += currentNum;
+        currentNum = previousNum;
+    } else if (operator === "-") {
+        previousNum -= currentNum;
+        currentNum = previousNum;
+    } else if (operator === "x") {
+        previousNum *= currentNum;
+        currentNum = previousNum;
     } else {
-        return "error";
+        if (currentNum <= 0) {
+            previousNum = "Error";
+            screen.textContent = previousNum;
+            return;
+        }
+        previousNum /= currentNum;
+        currentNum = previousNum;
+    }
+
+    previousNum = roundNum(previousNum);
+    
+    previousNum = previousNum.toString();
+    currentNum = currentNum.toString();
+}
+
+function roundNum(num) {
+    return Math.round(num * 1000) / 1000;
+}
+
+function addDecimal() {
+    if(!currentNum.includes(".")) {
+        currentNum += ".";
     }
 }
